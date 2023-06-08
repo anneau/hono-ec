40e0782fc65f512e2fb3f6dc6442c74dfa21d0ba
anneau <hkobori@gmx.com>
2023-06-08 06:30:08 +0000

    ✅ Add create account dto

--- src/application/usecase/create-account.ts
+++ src/application/usecase/create-account.ts
@@ -2,6 +2,18 @@ import { Account } from "../../domain/model/account";
 import { AccountRepository } from "../../infrastructure/repository/account";
 import { v4 as uuidv4 } from "uuid";
 
+export class CreateAccountDTO {
+  public readonly id: string;
+  public readonly name: string;
+  public readonly email: string;
+
+  constructor(id: string, name: string, email: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+}
+
 export class CreateAccount {
   private repository: AccountRepository;
 
@@ -11,6 +23,7 @@ export class CreateAccount {
 
   async execute(name: string, email: string, password: string) {
     const account = new Account({ id: uuidv4(), name, email, password });
-    return await this.repository.create(account);
+    const created = await this.repository.create(account);
+    return new CreateAccountDTO(created.id, created.name, created.email);
   }
 }
